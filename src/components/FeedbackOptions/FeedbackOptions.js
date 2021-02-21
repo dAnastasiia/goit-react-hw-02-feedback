04ce1e6b7272023c2d69345c5866119c39e221a0
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-21 15:09:42 +0200

    create component FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,2 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './FeedbackOptions.scss';
+
+class FeedbackOptions extends Component {
+  static propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+
+    return options.map(name => (
+      <button
+        type="button"
+        className="button"
+        name={name}
+        onClick={onLeaveFeedback}
+        key={name}
+      >
+        {name}
+      </button>
+    ));
+  }
+}
+
+export default FeedbackOptions;
